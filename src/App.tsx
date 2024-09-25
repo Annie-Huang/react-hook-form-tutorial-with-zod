44b3d969c6a74ff034049bce8d31fe7c53e49c44
Annie Huang <annie.hn@gmail.com>
2024-09-25 22:06:53 +1000

    Copy 1-simple-form.tsx into 2-with-react-hook-form.tsx and change component name to be ReactHookForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import SimpleForm from './components/1-simple-form.tsx';
+import ReactHookForm from './components/2-with-react-hook-form.tsx';
 
 function App() {
 
   return (
-    <div>      <h1>React Hook Form Tutorial</h1>
-      <SimpleForm /></div>
+    <div>
+      <h1>React Hook Form Tutorial</h1>
+      {/*<SimpleForm/>*/}
+      <ReactHookForm/>
+    </div>
   )
 }
 
