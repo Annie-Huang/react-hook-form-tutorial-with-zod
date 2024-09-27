3a2597e975f9b9af75f24e7ed486e009a888fc1f
Annie Huang <annie.hn@gmail.com>
2024-09-27 16:55:27 +1000

    Use it under App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import SimpleForm from './components/1-simple-form.tsx';
 import ReactHookForm from './components/2-with-react-hook-form.tsx';
+import ReactHookFormWithZod from './components/3-react-hook-form-with-zod.tsx';
 
 function App() {
   return (
     <div>
       <h1>React Hook Form Tutorial</h1>
       {/*<SimpleForm/>*/}
-      <ReactHookForm />
+      {/*<ReactHookForm />*/}
+      <ReactHookFormWithZod />
     </div>
   );
 }
